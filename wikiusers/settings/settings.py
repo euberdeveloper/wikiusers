9e01ea50f40fa502886b0b21a63248dddadde71f
euberdeveloper <euberdeveloper@gmail.com>
2021-07-07 17:37:44 +0200

    refactor: change default daasets dir
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- wikiusers/settings/settings.py
+++ wikiusers/settings/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import multiprocessing
 
-DEFAULT_DATASETS_DIR = Path.cwd().joinpath('datasets') if __name__ != '__main__' else Path(__file__).parent.parent.parent.joinpath('datasets')
+DEFAULT_DATASETS_DIR = Path.home().joinpath('.wikiusers').joinpath('datasets') if __name__ != '__main__' else Path(__file__).parent.parent.parent.joinpath('datasets')
 DEFAULT_DATABASE_PREFIX = 'wikiusers'
 DEFAULT_LANGUAGE = 'it'
 DEFAULT_SYNC_DATA = True
@@ -11,6 +11,3 @@ DEFAULT_BATCH_SIZE = int(2e4)
 DEFAULT_FORCE = False
 DEFAULT_SKIP = False
 DEFAULT_ERASE_DATASETS = False
-DEFAULT_DROPOFF_MONTH_THRESHOLD = 12
-DEFAULT_METRICS_DIR = Path.cwd().joinpath('metrics') if __name__ != '__main__' else Path(__file__).parent.parent.parent.joinpath('metrics')
-DEFAULT_ACTIVE_PER_MONTH_THRESHOLD = 5
